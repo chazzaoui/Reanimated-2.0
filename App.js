f3fe5144af139f166ea3c2358c80d7afc0d17b2b
chazzaoui <c.azzaoui@live.nl>
2020-06-20 17:16:11 +0200

    (feat): first commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+} from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet } from 'react-native'
+
+export default function App() {
+  const x = useSharedValue(0);
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startX = x.value;
+    },
+    onActive: (event, ctx) => {
+      x.value = ctx.startX + event.translationX;
+    },
+    onEnd: _ => {
+      x.value = withSpring(0);
+    },
+  });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: x.value,
+        },
+      ],
+    };
+  });
+
+  return (
+    <PanGestureHandler onGestureEvent={gestureHandler}>
+      <Animated.View style={[styles.box, animatedStyle]} />
+    </PanGestureHandler>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black'
+  }
+})
---
+++ src/components/AnimatedHelpers/index.ts
@@ -0,0 +1,7 @@
+export * from "./Math";
+export * from "./Animations";
+export * from "./Coordinates";
+export * from "./Hooks";
+export * from "./Transform";
+export * from "./Colors";
+export { default as ReText } from "./ReText";
